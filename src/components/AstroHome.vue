5be6b2b12686350b5416b999fc0f9d838f83a2fe
潘安悦 <15968355+pan-anyue@user.noreply.gitee.com>
2026-09-13 21:43:25 +0800

    migrate homepage UI to Astro

---
+++ src/components/AstroHome.vue
@@ -0,0 +1,18 @@
+<template>
+  <GlassyBackground>
+    <div class="relative z-10 flex min-h-screen flex-col">
+      <Navbar />
+      <main class="mx-auto w-full max-w-7xl flex-1 px-4 py-8 sm:px-6 sm:py-10 lg:px-8 lg:py-14">
+        <HomeView />
+      </main>
+      <Footer />
+    </div>
+  </GlassyBackground>
+</template>
+
+<script setup>
+import Navbar from './Navbar.vue'
+import Footer from './Footer.vue'
+import HomeView from '../views/HomeView.vue'
+import GlassyBackground from './ui/GlassyBackground.vue'
+</script>
